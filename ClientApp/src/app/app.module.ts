3976bf22a6124ae540dfb93642e6f9d6a00947a0
Anatoliy Novikov <chamskra66er@gmail.com>
2020-04-09 08:29:50 +0300

    added model.modul

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -4,14 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ModelModule } from './models/model.modul';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, ModelModule],
   providers: [],
   bootstrap: [AppComponent]
 })
